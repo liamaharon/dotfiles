431e5a074faeed48680d10531ade2339d519a1d7
Liam Aharon <liam.aharon@hotmail.com>
2023-12-14 15:44:32 +0400

    override conceallevel

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,2 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+
+local opt = vim.opt
+
+opt.conceallevel = 0
