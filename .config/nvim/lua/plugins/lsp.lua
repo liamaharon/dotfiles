fca3376c53f3f0dc2273935332849220cc8a8fbf
Liam Aharon <liam.aharon@hotmail.com>
2023-12-24 15:23:00 +0400

    more idiomatic rust-analyzer config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -4,31 +4,38 @@ return {
     servers = {
       taplo = { enabled = false },
       rust_analyzer = {
-        cmd = { "/Users/liamaharon/grimoire/ra-multiplex/target/release/ra-multiplex", "client" },
+        -- disable ra-multiplex until it gets more stable
+        -- cmd = { "/Users/liamaharon/grimoire/ra-multiplex/target/release/ra-multiplex", "client" },
         settings = {
           ["rust-analyzer"] = {
-            cargo = {
-              features = "all",
+            rust = {
+              analyzerTargetDir = "target/nvim-rust-analyzer",
+            },
+            server = {
               extraEnv = {
-                ["CARGO_TARGET_DIR"] = "target/nvim-rust-analyzer",
-                ["SKIP_WASM_BUILD"] = "1",
                 ["CHALK_OVERFLOW_DEPTH"] = "100000000",
+                ["CHALK_SOLVER_MAX_SIZE"] = "100000000",
               },
+            },
+            cargo = {
+              extraEnv = {
+                ["SKIP_WASM_BUILD"] = "1",
+              },
+              features = "all",
             },
             check = {
               allTargets = true,
               command = "check",
             },
             checkOnSave = true,
-            -- procMacro = {
-            --   ignored = {
-            --     -- disable these if we get weird errors compiling
-            --     ["async-trait"] = { "async_trait" },
-            --     ["napi-derive"] = { "napi" },
-            --     ["async-recursion"] = { "async_recursion" },
-            --     ["async-std"] = { "async_std" },
-            --   },
-            -- },
+            procMacro = {
+              ignored = {
+                ["async-trait"] = { "async_trait" },
+                ["napi-derive"] = { "napi" },
+                ["async-recursion"] = { "async_recursion" },
+                ["async-std"] = { "async_std" },
+              },
+            },
             rustfmt = {
               extraArgs = { "+nightly-2023-11-01" },
             },
