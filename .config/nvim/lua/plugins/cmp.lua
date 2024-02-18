e1f78ff37cd50fdc7cd1bd9693a0dcfc82759447
Liam Aharon <liam.aharon@hotmail.com>
2024-02-18 19:22:43 +1100

    nvim-cmp: only confirm explicitly selected items

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -7,20 +7,31 @@ return {
 
   {
     "hrsh7th/nvim-cmp",
-    opts = {
-      -- don't auto select items (expanding) in the completion menu
-      completion = {
-        completeopt = "menu,menuone,noselect",
-      },
-      -- make sure lsp suggestions are always first
-      sources = require("cmp").config.sources({
-        { name = "copilot" },
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "path" },
-      }, {
-        { name = "buffer" },
-      }),
-    },
+    opts = function()
+      local cmp = require("cmp")
+      return {
+        -- don't auto select items (expanding) in the completion menu
+        completion = {
+          completeopt = "menu,menuone,noselect",
+        },
+        -- override `select` to `false` to only confirm explicitly selected items
+        mapping = {
+          ["<CR>"] = cmp.mapping.confirm({ select = false }),
+          ["<S-CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = false,
+          }),
+        },
+        -- override suggestion source ordering
+        sources = require("cmp").config.sources({
+          { name = "copilot" },
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "path" },
+        }, {
+          { name = "buffer" },
+        }),
+      }
+    end,
   },
 }
