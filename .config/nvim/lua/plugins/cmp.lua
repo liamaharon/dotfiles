8fcd896f6d3522a5e66757835d84bfc2fe4ff1c2
Liam Aharon <liam.aharon@hotmail.com>
2024-02-22 15:45:51 +1100

    require explicitly confirming cmp items

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -2,35 +2,59 @@ return {
   -- disable annoying snippets
   {
     "rafamadriz/friendly-snippets",
-    enabled = false,
+    enabled = true,
   },
 
   {
     "hrsh7th/nvim-cmp",
     opts = function()
+      vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
       local cmp = require("cmp")
+      local defaults = require("cmp.config.default")()
       return {
         -- don't auto select items (expanding) in the completion menu
         completion = {
           completeopt = "menu,menuone,noselect",
         },
-        -- override `select` to `false` to only confirm explicitly selected items
-        mapping = {
+        mapping = cmp.mapping.preset.insert({
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = false }),
           ["<S-CR>"] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Replace,
             select = false,
           }),
-        },
+          ["<C-CR>"] = function(fallback)
+            cmp.abort()
+            fallback()
+          end,
+        }),
         -- override suggestion source ordering
         sources = require("cmp").config.sources({
           { name = "copilot" },
           { name = "nvim_lsp" },
           { name = "luasnip" },
           { name = "path" },
-        }, {
-          { name = "buffer" },
-        }),
+        }, { name = "buffer" }),
+        formatting = {
+          format = function(_, item)
+            local icons = require("lazyvim.config").icons.kinds
+            if icons[item.kind] then
+              item.kind = icons[item.kind] .. item.kind
+            end
+            return item
+          end,
+        },
+        experimental = {
+          ghost_text = {
+            hl_group = "CmpGhostText",
+          },
+        },
+        sorting = defaults.sorting,
       }
     end,
   },
