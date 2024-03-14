e3a5c1b0c6e304330b9d803556d3826e2d5b797f
Liam Aharon <liam.aharon@hotmail.com>
2024-03-14 12:31:09 +1100

    set root_spec to cwd

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,8 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 
-local opt = vim.opt
-
-opt.conceallevel = 0
+vim.opt.conceallevel = 0
 
 vim.g.minipairs_disable = true
+
+vim.g.root_spec = { "cwd" }
