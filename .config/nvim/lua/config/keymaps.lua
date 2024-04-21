f5c0d7d515ca325f3876c6997ca66927c4664126
Liam Aharon <liam.aharon@hotmail.com>
2024-04-21 12:38:21 +0400

    {}zzzv

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -10,6 +10,8 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "}", "}zzzv")
+vim.keymap.set("n", "{", "{zzzv")
 
 -- Crates package keymaps
 local crates = require("crates")
